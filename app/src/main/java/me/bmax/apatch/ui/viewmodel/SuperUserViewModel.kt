11b2d08389616d06b68f9acbde92a8d1718ebaa6
Cui Zhou <125462512+songyy0415@users.noreply.github.com>
2026-03-01 17:11:35 +0800

    Refactor SuperUserViewModel to use ApplicationInfo

--- app/src/main/java/me/bmax/apatch/ui/viewmodel/SuperUserViewModel.kt
+++ app/src/main/java/me/bmax/apatch/ui/viewmodel/SuperUserViewModel.kt
@@ -15,22 +15,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.topjohnwu.superuser.Shell
+import dev.utils.app.AppUtils.getPackageManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.parcelize.Parcelize
 import me.bmax.apatch.IAPRootService
 import me.bmax.apatch.Natives
 import me.bmax.apatch.apApp
-import me.bmax.apatch.services.RootServices
-import me.bmax.apatch.util.APatchCli
 import me.bmax.apatch.util.HanziToPinyin
 import me.bmax.apatch.util.PkgConfig
 import java.text.Collator
 import java.util.Locale
 import kotlin.concurrent.thread
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
+
 
 
 class SuperUserViewModel : ViewModel() {
@@ -48,12 +45,12 @@ class SuperUserViewModel : ViewModel() {
 
     @Parcelize
     data class AppInfo(
-        val label: String, val packageInfo: PackageInfo, val config: PkgConfig.Config
+        val label: String, val applicationInfo: ApplicationInfo, val config: PkgConfig.Config
     ) : Parcelable {
         val packageName: String
-            get() = packageInfo.packageName
+            get() = applicationInfo.packageName
         val uid: Int
-            get() = packageInfo.applicationInfo!!.uid
+            get() = applicationInfo.uid
     }
 
     var search by mutableStateOf("")
@@ -79,52 +76,21 @@ class SuperUserViewModel : ViewModel() {
                 .toPinyinString(it.label).contains(search.lowercase())
         }.filter {
             it.uid == 2000 // Always show shell
-                    || showSystemApps || it.packageInfo.applicationInfo!!.flags.and(ApplicationInfo.FLAG_SYSTEM) == 0
+                    || showSystemApps || it.applicationInfo.flags.and(ApplicationInfo.FLAG_SYSTEM) == 0
         }
     }
 
-    private suspend inline fun connectRootService(
-        crossinline onDisconnect: () -> Unit = {}
-    ): Pair<IBinder, ServiceConnection> = suspendCoroutine {
-        val connection = object : ServiceConnection {
-            override fun onServiceDisconnected(name: ComponentName?) {
-                onDisconnect()
-            }
-
-            override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
-                it.resume(binder as IBinder to this)
-            }
-        }
-        val intent = Intent(apApp, RootServices::class.java)
-        val task = RootServices.bindOrTask(
-            intent,
-            Shell.EXECUTOR,
-            connection,
-        )
-        val shell = APatchCli.SHELL
-        task?.let { it1 -> shell.execTask(it1) }
-    }
-
-    private fun stopRootService() {
-        val intent = Intent(apApp, RootServices::class.java)
-        RootServices.stop(intent)
-    }
+    
 
     suspend fun fetchAppList() {
         isRefreshing = true
 
-        try {
-            val result = connectRootService {
-                Log.w(TAG, "RootService disconnected")
-            }
+        
 
             withContext(Dispatchers.IO) {
-                val binder = result.first
-                val allPackages = IAPRootService.Stub.asInterface(binder).getPackages(0)
-
-                withContext(Dispatchers.Main) {
-                    stopRootService()
-                }
+                val pm = getPackageManager()
+                val allPackages = pm.getInstalledApplications(PackageManager.MATCH_UNINSTALLED_PACKAGES)
+                
                 val uids = Natives.suUids().toList()
                 Log.d(TAG, "all allows: $uids")
 
@@ -136,12 +102,12 @@ class SuperUserViewModel : ViewModel() {
 
                 Log.d(TAG, "all configs: $configs")
 
-                val newApps = allPackages.list.map {
-                    val appInfo = it.applicationInfo
-                    val uid = appInfo!!.uid
+                val newApps = allPackages.map {
+                    
+                    val uid = it.uid
                     val actProfile = if (uids.contains(uid)) Natives.suProfile(uid) else null
                     val config = configs.getOrDefault(
-                        uid, PkgConfig.Config(appInfo.packageName, Natives.isUidExcluded(uid), 0, Natives.Profile(uid = uid))
+                        uid, PkgConfig.Config(it.packageName, Natives.isUidExcluded(uid), 0, Natives.Profile(uid = uid))
                     )
                     config.allow = 0
 
@@ -151,8 +117,8 @@ class SuperUserViewModel : ViewModel() {
                         config.profile = actProfile
                     }
                     AppInfo(
-                        label = appInfo.loadLabel(apApp.packageManager).toString(),
-                        packageInfo = it,
+                        label = it.loadLabel(apApp.packageManager).toString(),
+                        applicationInfo = it,
                         config = config
                     )
                 }
